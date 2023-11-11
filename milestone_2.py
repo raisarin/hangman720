2b9d7e1393a2775f5d1303118de22a638c50a031
Sarin Rai <raisarin@yahoo.com>
2023-11-11 03:23:44 +0000

    Added user input & test for single letter

--- milestone_2.py
+++ milestone_2.py
@@ -5,3 +5,10 @@ print(word_list)
 
 word = random.choices(word_list)
 print(word)
+
+guess = input("Enter a single letter: ")
+
+if len(guess) == 1 and guess.isalpha():
+    print("Good guess!")
+else: 
+    print("Oops! That is not a valid input.")
