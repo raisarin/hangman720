771bc8a7a5e6a0797a2d79ea8036a25b28516244
Sarin Rai <raisarin@yahoo.com>
2023-11-13 15:27:46 +0000

    Updated milestone_4.py, added function if letter not in word

--- milestone_4.py
+++ milestone_4.py
@@ -10,14 +10,18 @@ class Hangman():
         self.list_of_guesses = []
     
     def check_guess(self, guess):
-        guess.lower()
+        guess = guess.lower()
         if guess in self.word:
             print(f"Good guess! {guess} is in the word.")
             for index, letter in enumerate(self.word): 
                 if letter == guess: 
                     self.word_guessed[index] = guess
             self.num_letters -= 1
-    
+        else: 
+            self.num_lives -= 1
+            print(f"Sorry, {guess} is not in the word.")
+            print(f"You have {self.num_lives} lives left.")
+
     def ask_for_input(self):
         while True: 
             guess = input("Guess a letter: ")
@@ -29,6 +33,7 @@ class Hangman():
                 self.check_guess(guess)
                 self.list_of_guesses.append(guess)
 
-            
-testing = Hangman(["Apple"])
+#word_list = ["apple", "banana", "cherry", "dragonfruit"]
+word_list = ["apple"]
+testing = Hangman(word_list)
 testing.ask_for_input()
