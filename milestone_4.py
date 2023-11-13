73f376654f6132d656b955b0f6824c6584270069
Sarin Rai <raisarin@yahoo.com>
2023-11-13 15:19:49 +0000

    Update check_guess; add guess into word_guess

--- milestone_4.py
+++ milestone_4.py
@@ -13,13 +13,17 @@ class Hangman():
         guess.lower()
         if guess in self.word:
             print(f"Good guess! {guess} is in the word.")
+            for index, letter in enumerate(self.word): 
+                if letter == guess: 
+                    self.word_guessed[index] = guess
+            self.num_letters -= 1
     
     def ask_for_input(self):
         while True: 
             guess = input("Guess a letter: ")
-            if len(guess) != 1 or guess.isalpha() == False: 
+            if len(guess) != 1 or not guess.isalpha(): 
                 print("Invalid letter. Please, enter a single alphabetical character.")
-            elif guess in self.list_of_guesses == True: 
+            elif guess in self.list_of_guesses: 
                 print("You already tried that letter!") 
             else: 
                 self.check_guess(guess)
